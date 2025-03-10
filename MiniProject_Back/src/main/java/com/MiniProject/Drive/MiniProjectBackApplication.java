700fc87fd17be12d5709571df7aad06a6ed2eb7b
seungwoo <zoozoo1302@gmail.com>
2025-03-10 17:20:53 +0900

    Spring 프로젝트

---
+++ MiniProject_Back/src/main/java/com/MiniProject/Drive/MiniProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.MiniProject.Drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectBackApplication.class, args);
+	}
+
+}
---
+++ MiniProject_Back/src/test/java/com/MiniProject/Drive/MiniProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MiniProject.Drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
