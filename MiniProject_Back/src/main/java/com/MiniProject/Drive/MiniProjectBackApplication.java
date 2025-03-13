436e318b54e27048e29fa6871cd922e3fc4b6eab
seungwoo <zoozoo1302@gmail.com>
2025-03-13 14:07:06 +0900

    보안

--- MiniProject_Back/src/main/java/com/MiniProject/Drive/MiniProjectBackApplication.java
+++ MiniProject_Back/src/main/java/com/MiniProject/Drive/MiniProjectBackApplication.java
@@ -2,8 +2,10 @@ package com.MiniProject.Drive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:config/secu.properties")
 public class MiniProjectBackApplication {
 
 	public static void main(String[] args) {
